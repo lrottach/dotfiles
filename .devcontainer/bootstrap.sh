e41bff863f1c14b7b2e8454c9a7bc327f4e21484
lrottach <lukas.rottach.ch@outlook.com>
2020-12-28 22:27:36 +0100

    fix: link for debian 10 nvm installation

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -10,7 +10,7 @@ apt-get install -y \
   wget
 
 ## Instsall nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
 ## Install Docker CE
 curl -fsSL https://get.docker.com | bash
