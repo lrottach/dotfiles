52514a9bbb522d8bc1fcebae55867d664985f556
lrottach <lukas.rottach.ch@outlook.com>
2020-12-28 23:21:22 +0100

    feat: zsh and function core tools v3 to bootstrap.sh

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -1,13 +1,14 @@
-## Update and install some cool tools
+## Update and install some stuff
 apt-get update
 apt-get install -y \
-  curl \
-  git \
-  gnupg2 \
-  jq \
-  sudo \
-  zsh \
-  wget
+    curl \
+    git \
+    gnupg2 \
+    jq \
+    sudo \
+    zsh \
+    wget \
+    apt-utils
 
 ## Instsall nvm
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
@@ -18,3 +19,21 @@ sudo sh get-docker.sh
 
 ## Update to the latest PowerShell
 curl -sSL https://raw.githubusercontent.com/PowerShell/PowerShell/master/tools/install-powershell.sh | bash
+
+## Setup and install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp -R /root/.oh-my-zsh /home/$USERNAME
+cp /root/.zshrc /home/$USERNAME
+sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
+chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+
+## Install Azure Functions core tools
+DEBIAN_VERSION=10
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/debian/$DEBIAN_VERSION/prod.list
+sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+sudo apt-get update
+sudo apt-get install azure-functions-core-tools-3
