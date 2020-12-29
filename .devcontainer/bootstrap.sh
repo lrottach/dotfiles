453bd6bc081c91d8a70a57bb4ad77b03aaf46913
lrottach <lukas.rottach.ch@outlook.com>
2020-12-29 15:50:04 +0100

    feat: added .NET 5.0 to the bootstrap process

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -10,7 +10,6 @@ apt-get install -y \
     wget \
     apt-transport-https \
     ca-certificates
-    software-properties-common
 
 ## Instsall nvm
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
@@ -22,21 +21,11 @@ cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
 
-## Install Azure Functions core tools
-DEBIAN_VERSION=10
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
-sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
-wget -q https://packages.microsoft.com/config/debian/$DEBIAN_VERSION/prod.list
-sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
-sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
-sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
-sudo apt-get update
-sudo apt-get install azure-functions-core-tools-3
+## Install .NET 5.0 SDK
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
 
-# Install Azure CLI
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-
-# Install Terraform
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-5.0
