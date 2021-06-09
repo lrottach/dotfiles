de47411fe918b7f7d4d6044402266153e0f1952c
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2021-06-09 15:51:02 +0200

    fix: broken font zip file name

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 # Install Jetbrains Mono font
 wget https://download.jetbrains.com/fonts/JetBrainsMono-2.225.zip
-sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
+sudo unzip JetBrainsMono-2.225.zip -d /usr/share/fonts
 sudo fc-cache -f -v
