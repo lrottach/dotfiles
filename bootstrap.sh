13595fcfba205524ce59ba209fbb2ca87ce4f482
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2021-06-09 15:50:31 +0200

    refactor: removed package installations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,3 @@
-## update and install required packages
-sudo apt-get update
-sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
-sudo apt-get install -y \
-  curl \
-  git \
-  gnupg2 \
-  jq \
-  sudo \
-  openssh-client \
-  less \
-  iproute2 \
-  procps \
-  wget \
-  unzip \
-  apt-transport-https \
-  lsb-release 
-
 # Install Jetbrains Mono font
 wget https://download.jetbrains.com/fonts/JetBrainsMono-2.225.zip
 sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
