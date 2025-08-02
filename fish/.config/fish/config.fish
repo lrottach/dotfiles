2d62b61f6790a9ddf75548e9dd9c534e9aa36166
Lukas Rottach <50323692+lrottach@users.noreply.github.com>
2025-08-02 16:29:51 +0200

    feat: add zoxide init to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-	atuin init fish | source
-	starship init fish | source
+    atuin init fish | source
+    starship init fish | source
+    zoxide init fish | source
 end
 
 alias claude="/Users/lukas.rottach/.claude/local/claude"
